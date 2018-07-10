8ef2ab4e8a889ba07bb268ad8a2d00f20f44a70c
Kaden <you@example.com>
2018-07-10 11:10:48 -0600

    ugh

--- src/components/app.js
+++ src/components/app.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
+import Picker from './picker'
 
 export default class App extends Component {
   render() {
     return (
       <div className="grid">
-        <div className="grid__skew-dark-one-box"></div>
-        <div className="grid__skew-dark-two"></div>
-        <div className="grid__skew-dark-three"></div>
+        <h1 className="grid__title">Birthday Countdown</h1>
 
-        <div className="grid__skew-light-one"></div>
-        <div className="grid__skew-light-two"></div>
-        <div className="grid__skew-light-box"></div>
+        <div className="grid__skew-dark"></div>
+
+        <div className="grid__skew-light"></div>
+
+        <Picker/>
       </div>
     );
   }
